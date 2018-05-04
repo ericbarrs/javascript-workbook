1121fb2ea727096809054177b10da2292b922c49
ericbarrs <yellastar42@yahoo.com>
2018-05-03 19:41:21 -0500

    finished

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const arr1 = ['eric','jonny','trey','gina','shaun'];
+const arr1 = ['eric', 'jonny', 'trey', 'gina', 'shaun'];
 
 const printListOlympians = (arr1) => {
-arr1.forEach(function(arr1){
-  console.log(arr1);
-})
+  arr1.forEach(function(arr1) {
+    console.log(arr1);
+  })
 }
 
 printListOlympians(arr1);
